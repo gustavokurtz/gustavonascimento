41d8829521f710404f5a2a2444707da39eb83955
kurtz <91634598+kurtzfinalboss@users.noreply.github.com>
2023-08-09 15:34:49 -0300

    mudança padding

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -5,11 +5,8 @@ const AboutContainer = () => {
         <p>
         Como um jovem desenvolvedor apaixonado pela programação desde os 16 anos, com apenas 17 anos de idade, mergulhei de cabeça no mundo do desenvolvimento front-end e back end. Através do domínio de tecnologias como JavaScript, NodeJs, HTML e CSS, busco constantemente aprimorar minhas habilidades. Expandindo meu leque de conhecimentos e buscando acompanhar as tendências mais atuais no desenvolvimento web. Minha sede de evolução e aprendizado me impulsiona a sempre estar em busca de novos desafios e oportunidades para me aprimorar como desenvolvedor e criar experiências web 
         impactantes.
-
         </p>
-        <p>
-        Comprometido com a aprendizagem contínua, estou sempre em busca de oportunidades para aprimorar minhas habilidades e criar experiências web impactantes.
-        </p>
+       
       </section>
     );
   };
