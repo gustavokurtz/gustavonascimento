424b48e4e8669d1c0f16c07a73099d93edb4d584
kurtzfinalboss <kurtzff32@gmail.com>
2023-08-30 00:16:04 -0300

    github alteration

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -5,7 +5,7 @@ const ProjectsContainer = () => {
         <p>
         Meu portfólio apresenta uma coleção de projetos envolvendo JavaScript, NodeJs, HTML, CSS e React, demonstrando minha paixão pela criação de experiências web interativas e responsivas.
         </p>
-        <a href="https://github.com/kurtzfinalboss" className="btn">
+        <a href="https://github.com/gustavokurtz" className="btn">
           Ver Projetos
         </a>
       </section>
