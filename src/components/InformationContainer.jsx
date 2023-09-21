7b77a063c32e7cd8efc714081ee94bc41a8eedba
kurtzfinalboss <kurtzff32@gmail.com>
2023-09-21 14:51:42 -0300

    add hyperlink in project

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -1,22 +1,33 @@
 import "../styles/components/informationcontainer.sass";
-
 import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from "react-icons/ai";
 
 const InformationContainer = () => {
+  // Número de telefone do WhatsApp no formato internacional
+  const phoneNumber = "+5532999165667"; 
+  const Email = "gustavoocontact@gmail.com"
+
+  // URL para o WhatsApp
+  const whatsappURL = `https://wa.me/${phoneNumber}`;
+
   return (
     <section id="information">
       <div className="info-card">
         <AiFillPhone id="phone-icon" />
         <div>
           <h3>Telefone</h3>
-          <p>(32)99916-5667</p>
+          {/* Adicione um hyperlink para o WhatsApp */}
+          <a className="hyperLink" href={whatsappURL} target="_blank" rel="noopener noreferrer">
+            (32)99916-5667
+          </a>
         </div>
       </div>
       <div className="info-card">
         <AiOutlineMail id="email-icon" />
         <div>
+          <div>
           <h3>E-mail</h3>
           <p>gustavoocontact@gmail.com</p>
+        </div>
         </div>
       </div>
       <div className="info-card">
