423dba89f89bf094bbbe9014973171c0fdd8b361
kurtzfinalboss <kurtzff32@gmail.com>
2023-09-22 00:16:10 -0300

    add hyperlink in email and location, is add a border in the hyperlink

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -3,11 +3,16 @@ import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from "react-icons/ai";
 
 const InformationContainer = () => {
   // Número de telefone do WhatsApp no formato internacional
-  const phoneNumber = "+5532999165667"; 
-  const Email = "gustavoocontact@gmail.com"
+  const phoneNumber = "+5532999165667";
 
   // URL para o WhatsApp
   const whatsappURL = `https://wa.me/${phoneNumber}`;
+
+  // URL para abrir o Google Maps com a localização da sua cidade
+  const googleMapsURL = "https://www.google.com/maps/place/Dores+de+Campos,+MG";
+
+  // URL para o Gmail com o endereço de e-mail preenchido
+  const gmailURL = `https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=gustavoocontact@gmail.com`;
 
   return (
     <section id="information">
@@ -26,15 +31,20 @@ const InformationContainer = () => {
         <div>
           <div>
           <h3>E-mail</h3>
-          <p>gustavoocontact@gmail.com</p>
-        </div>
+          <a className="hyperLink" href={gmailURL} target="_blank" rel="noopener noreferrer">
+            gustavoocontact@gmail.com
+          </a>
+          </div>
         </div>
       </div>
       <div className="info-card">
         <AiFillEnvironment id="pin-icon" />
         <div>
           <h3>Localização</h3>
-          <p>Dores de Campos / MG</p>
+          {/* Adicione um hyperlink para o Google Maps */}
+          <a className="hyperLink" href={googleMapsURL} target="_blank" rel="noopener noreferrer">
+            Dores de Campos / MG
+          </a>
         </div>
       </div>
     </section>
