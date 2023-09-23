e2a0eb5cd2bfcc10d1907563eea06adbe7fc25d8
kurtzfinalboss <kurtzff32@gmail.com>
2023-09-23 00:20:19 -0300

    correction of words

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,7 +3,7 @@ const AboutContainer = () => {
       <section className="about-container">
         <h2>Sobre</h2>
         <p>
-        Como um jovem desenvolvedor apaixonado pela programação desde os 16 anos, mergulhei de cabeça no mundo do desenvolvimento front-end e back-end. Através do domínio de tecnologias como JavaScript, HTML e CSS, ReactJS, NodeJs, TypeScript, NestJs, Prisma, busco constantemente aprimorar minhas habilidades, expandindo meu leque de conhecimentos e buscando acompanhar as tendências mais atuais no desenvolvimento web.
+        Como um jovem desenvolvedor apaixonado pela programação desde os 16 anos, mergulhei de cabeça no mundo do desenvolvimento front-end e back-end. Através do domínio de tecnologias como JavaScript, HTML e CSS, ReactJS, NodeJs, TypeScript, NestJs e Prisma. Busco constantemente aprimorar minhas habilidades, expandindo meu leque de conhecimentos e buscando acompanhar as tendências mais atuais no desenvolvimento web.
         </p>
        
       </section>
